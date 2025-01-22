fca1cdb11e47dc684bd391ea49e70797d95348bc
nick-fite <70393136+nick-fite@users.noreply.github.com>
2025-01-21 22:36:09 -0600

    started splat

--- GGJProxy/Assets/SplatterCleanUp.cs
+++ GGJProxy/Assets/SplatterCleanUp.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class SplatterCleanUp : MonoBehaviour
 {
-    [SerializeField] Texture2D bloodMaskTexture;
+    [SerializeField] Texture2D floorTexture;
     void Start()
     {
 
@@ -23,54 +23,25 @@ public class SplatterCleanUp : MonoBehaviour
                 //Vector2Int textCoord = new Vector2Int((int)hit.textureCoord.x * bloodMaskTexture.width, (int)hit.textureCoord.y * bloodMaskTexture.height);
                 Material mat = hit.collider.GetComponent<Renderer>().material;
                 Texture2D tex = mat.mainTexture as Texture2D;
-                Texture2D newTex = tex;
                 
                 Vector2 pixelUV = hit.textureCoord;
                 pixelUV.x *= tex.width;
                 pixelUV.y *= tex.height;
 
                 Vector2Int start = new Vector2Int((int)pixelUV.x -5, (int)pixelUV.y -5);
-                List<Color32> test = new List<Color32>();
+                List<Color32> newColors = new List<Color32>();
                 for(int i = 0; i < 10; i++)
                 {
                     for(int j = 0; j < 10; j++)
                     {
-                        test.Add(Color.black);
+                        Color color = floorTexture.GetPixel(start.x + i, start.y + j);
+                        newColors.Add(color);
                     }
                 }
-                tex.SetPixels32(start.x, start.y, 10,10, test.ToArray());
+                tex.SetPixels32(start.x, start.y, 10,10, newColors.ToArray());
 
                 tex.Apply();
-
-                //Texture2D mask = mat.GetTexture("_BloodMask") as Texture2D;
-                //Vector2Int start = new Vector2Int(textCoord.x-5, textCoord.y -5);
-                //for(int i = 0; i < 100; i++)
-                //{
-
-                //}
-
-                //mask.SetPixel(textCoord.x, textCoord.y, Color.black);
-                //mat.SetTexture("_BloodMask",bloodMaskTexture);
-                //bloodMaskTexture.Apply();
-                
-                //Texture2D newMask = mask;
-
-                /*Texture2D maskBase = mat.GetTexture("_BloodMask") as Texture2D;
-                Color bloodPixel = maskBase.GetPixel(textCoord.x, textCoord.y);
-                Debug.Log(bloodPixel);
-                Texture2D mask = new Texture2D(maskBase.width, maskBase.height);
-                mask.SetPixels(maskBase.GetPixels());
-                mask.SetPixel(textCoord.x, textCoord.y, Color.black);
-                mask.Apply();
-                mat.SetTexture("_BloodMask", mask);*/
             }
-            
-
-            //Vector2 textCoord = hit.textureCoord;
-            //int pixelX = (int)(textCoord.x * bloodMaskTexture.width);
-            //int pixelY = (int)(textCoord.x * bloodMaskTexture.height);
-
-            //Vector2Int paintPixelPos = new Vector2Int(pixelX, pixelY);
         }
     }
 }
---
+++ GGJProxy/Assets/testAssets/CleanUp/SplatScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class SplatScript : MonoBehaviour
+{
+    [SerializeField]Texture2D splatTexture;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Splat()
+    {
+        
+    }
+}
